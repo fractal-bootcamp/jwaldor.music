26333222abd82e6a66f7c8a3fb6f8dc5723615ba
jwaldor <98110682+jwaldor@users.noreply.github.com>
2024-08-23 10:48:08 -0400

    more layout & styling progress

--- src/App.tsx
+++ src/App.tsx
@@ -117,70 +117,115 @@ function App() {
   return (
     <>
       <body>
-        <div>
+        <div className="max-h-full">
+          {/* <div id="player-info">
+            <h3 id="track-name">Track Name</h3>
+            <p id="artist-name">Artist Name</p>
+            <img id="album-art" src="" alt="Album Art" width="200px" />
+          </div>
           <h1>Spotify Player</h1>
-          <button id="login">Login to Spotify</button>
-          <div className="flex flex-row min-w-[100%] relative left-60 pb-3">
-            <input
-              type="text"
-              placeholder="Search"
-              className="input input-bordered w-full max-w-xs"
-            />
+          <button id="login">Login to Spotify</button> */}
+          <div className="bg-base-100">
+            <div className="flex flex-row">
+              <a className="btn btn-ghost text-xl">Tidal</a>
+              <div className="form-control w-[50%] ml-auto mr-60 mb-5">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className="input input-bordered w-24 md:w-auto"
+                />
+              </div>
+              <div className="dropdown dropdown-end">
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost btn-circle avatar"
+                >
+                  <div className="w-10 rounded-full">
+                    <img
+                      alt="Tailwind CSS Navbar component"
+                      src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                    />
+                  </div>
+                </div>
+                <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+                >
+                  <li>
+                    <a className="justify-between">
+                      Profile
+                      <span className="badge">New</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a>Settings</a>
+                  </li>
+                  <li>
+                    <a>Logout</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="flex-none gap-2"></div>
           </div>
           <div className="flex flex-row text-blue-200">
-            <div className="flex flex-col">
-              <ul className="menu bg-base-200 rounded-box w-56">
-                <li>
-                  <a>Item 1</a>
-                </li>
-                <li>
-                  <a>Item 2</a>
-                </li>
-                <li>
-                  <a>Item 3</a>
-                </li>
-              </ul>
-            </div>
-            <div className="flex flex-col min-h-screen max-h-screen">
-              <div className="overflow-x-auto min-w-">
-                <table className="table">
+            {/* <div className="flex flex-col w-[20%] mr-10 mt-10 relative min-h-[100%]"> */}
+            <ul className="menu  w-56 rounded-l-none rounded-br-none mt-3 border border-zinc-300 border-l-0 rounded-tr-lg">
+              <li className="menu-title font-light">Playlists</li>
+              <li className="font-light">
+                <a>Item 1</a>
+              </li>
+              <li className="font-light">
+                <a>Item 2</a>
+              </li>
+            </ul>
+            {/* </div> */}
+            <div className="flex flex-col w-full min-h-screen max-h-screen">
+              {/* <div className="flex flex-row min-w-screen relative pb-3 ml-2 mt-4">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className="input input-bordered w-full max-w-xs"
+                />
+              </div> */}
+              <div className="overflow-x-auto">
+                <table className="table-md min-w-[60%]">
                   {/* head */}
-                  <thead>
+                  <thead className="font-light">
                     <tr>
                       <th></th>
-                      <th>Name</th>
-                      <th>Job</th>
-                      <th>Favorite Color</th>
+                      <th className="font-light text-neutral-400">Title</th>
+                      <th className="font-light text-neutral-400">Artist</th>
+                      <th className="font-light text-neutral-400">Album</th>
                     </tr>
                   </thead>
                   <tbody>
                     {/* row 1 */}
-                    <tr>
+                    <tr className="text-neutral-300">
                       <th>1</th>
                       <td>Cy Ganderton</td>
                       <td>Quality Control Specialist</td>
                       <td>Blue</td>
                     </tr>
-                    {/* row 2 */}
-                    <tr>
+                    {/* <tr>
                       <th>2</th>
                       <td>Hart Hagerty</td>
                       <td>Desktop Support Technician</td>
                       <td>Purple</td>
                     </tr>
-                    {/* row 3 */}
                     <tr>
                       <th>3</th>
                       <td>Brice Swyre</td>
                       <td>Tax Accountant</td>
                       <td>Red</td>
-                    </tr>
+                    </tr> */}
                   </tbody>
                 </table>
               </div>
-              <PausePlay toggleplay={toggleplay} pauseplay={pauseplay} />
             </div>
           </div>
+          <PausePlay toggleplay={toggleplay} pauseplay={pauseplay} />
         </div>
       </body>
     </>
--- src/components/PausePlay.tsx
+++ src/components/PausePlay.tsx
@@ -8,79 +8,80 @@ export default function PausePlay({
   console.log(pauseplay, "pauseplay");
   return (
     <>
-      <div className="flex flex-row self-center justify-center border border-gray-100 p-3 rounded-xl min-w-48">
-        <button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M21 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061A1.125 1.125 0 0 1 21 8.689v8.122ZM11.25 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061a1.125 1.125 0 0 1 1.683.977v8.122Z"
-            />
-          </svg>
-        </button>
+      <div className="flex flex-row m-auto justify-center">
+        <div className="flex flex-row mt-auto mb-3 justify-self-center justify-center border border-gray-100 p-3 rounded-xl w-[30%]">
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061A1.125 1.125 0 0 1 21 8.689v8.122ZM11.25 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061a1.125 1.125 0 0 1 1.683.977v8.122Z"
+              />
+            </svg>
+          </button>
 
-        <button className="btn btn-circle" id="play-pause" onClick={toggleplay}>
-          {pauseplay === "pause" && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
-              />
-            </svg>
-          )}
-          {pauseplay === "play" && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M15.75 5.25v13.5m-7.5-13.5v13.5"
-              />
-            </svg>
-          )}
-        </button>
-        <button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="size-6"
+          <button
+            className="btn btn-circle"
+            id="play-pause"
+            onClick={toggleplay}
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M3 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061A1.125 1.125 0 0 1 3 16.811V8.69ZM12.75 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061a1.125 1.125 0 0 1-1.683-.977V8.69Z"
-            />
-          </svg>
-        </button>
-      </div>
-      <div id="player-info">
-        <h3 id="track-name">Track Name</h3>
-        <p id="artist-name">Artist Name</p>
-        <img id="album-art" src="" alt="Album Art" width="200px" />
+            {pauseplay === "pause" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
+                />
+              </svg>
+            )}
+            {pauseplay === "play" && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 5.25v13.5m-7.5-13.5v13.5"
+                />
+              </svg>
+            )}
+          </button>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061A1.125 1.125 0 0 1 3 16.811V8.69ZM12.75 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061a1.125 1.125 0 0 1-1.683-.977V8.69Z"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
     </>
   );
